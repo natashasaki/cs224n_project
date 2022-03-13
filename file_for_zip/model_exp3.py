c96374fec06b5a089d3352c0af688aaa07777bb5
Natasha Ong <56423291+natashasaki@users.noreply.github.com>
2022-03-12 22:31:52 -0800

    Update model_exp3.py

--- file_for_zip/model_exp3.py
+++ file_for_zip/model_exp3.py
@@ -5,26 +5,26 @@ class BertClassifier(nn.Module):
   def __init__(self, outputDim, freeze_bert_pretrained = True):
     super(BertClassifier, self).__init__()
 
-    D_in, H, D_out = 768, 256, outputDim
+    dim_in, H, dim_out = 768, 256, outputDim
 
     self.bert = BertModel.from_pretrained('bert-base-uncased')
 
     self.classifier = nn.Sequential(
-        nn.Linear(D_in, H),
+        nn.Linear(dim_in, H),
         nn.ReLU(),
-        nn.Linear(H, D_out)
+        nn.Linear(H, dim_out)
     )
 
 
-    if freeze_bert_pretrained:
+    if freeze_bert_pretrained: # only alter finetuning layers!
       for param in self.bert.parameters():
         param.requires_grad = False
   
   def forward(self, input_ids, attention_mask):
     outputs = self.bert(input_ids = input_ids, attention_mask = attention_mask)
-    first_hidden_state_cls = outputs[0][:, 0, :]
+    output = outputs[0][:, 0, :]
 
-    logits = self.classifier(first_hidden_state_cls)
-    sigmoid = nn.Sigmoid()
-    logits = sigmoid(logits)
+    logits = self.classifier(output)
+    #sigmoid = nn.Sigmoid()
+    #logits = sigmoid(logits)
     return logits
